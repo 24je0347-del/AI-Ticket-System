8ad8a0c1c7edae1f372b7461162d368a9c065791
Sonya Sen <24je0347@iitism.ac.in>
2026-06-25 15:32:36 +0530

    Initial full-stack AI ticket assistant setup

---
+++ ai-ticket-frontend/src/main.jsx
@@ -0,0 +1,57 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import CheckAuth from './components/check-auth.jsx'
+import Tickets from "./pages/tickets.jsx";
+import TicketDetailsPage from "./pages/ticket.jsx";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route
+      path='/'
+      element={
+        <CheckAuth protected={true}>
+          <Tickets/>
+        </CheckAuth>
+      }
+      />
+      <Route
+      path="/tickets/:id"
+      element={
+        <CheckAuth protected={true}>
+          <TicketDetailsPage/>
+        </CheckAuth>
+      }
+      />
+      <Route
+      path="/login"
+      element={
+        <CheckAuth protected={false}>
+          <Login/>
+        </CheckAuth>
+      }
+      />
+      <Route
+      path="/signup"
+      element={
+        <CheckAuth protected={false}>
+          <Signup/>
+        </CheckAuth>
+      }
+      />
+      <Route
+      path="/admin"
+      element={
+        <CheckAuth protected={true}>
+          <Admin/>
+        </CheckAuth>
+      }
+      />
+    </Routes>
+    </BrowserRouter>
+
+  </StrictMode>,
+);
